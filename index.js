daf0928f72ac516d479d343e13ff2b93f4096eeb
andrewlimhj <andrewlim.hj@gmail.com>
2022-04-25 16:40:40 +0800

    added new cats path

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
+import pg from 'pg';
+
+const { Pool } = pg;
+
+let pgConnectionConfigs;
+
+// test to see if the env var is set. Then we know we are in Heroku
+if (process.env.DATABASE_URL) {
+  // pg will take in the entire value and use it to connect
+  pgConnectionConfigs = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+} else {
+  // this is the same value as before
+  pgConnectionConfigs = {
+    user: 'andrewlim',
+    host: 'localhost',
+    database: 'andrewlim',
+    port: 5432,
+  };
+}
+const pool = new Pool(pgConnectionConfigs);
 
 const envFilePath = '.env';
 dotenv.config({ path: path.normalize(envFilePath) });
@@ -20,6 +45,20 @@ app.get('/bananas', (request, response) => {
   const data = { responseText };
 
   response.render('bananas', data);
+});
+
+app.get('/cats', (request, response) => {
+  console.log('request came in');
+  pool
+    .query('SELECT * from cats')
+    .then((result) => {
+      console.log(result.rows[0].name);
+      response.send(result.rows);
+    })
+    .catch((error) => {
+      console.error('Error executing query', error.stack);
+      response.status(503).send(result.rows);
+    });
 });
 
 app.listen(PORT);
